64d2f3e97245711de49de08e9a040f0c16212a8c
ernokanst <ernokanst@gmail.com>
2024-02-15 14:40:07 +0300

    Позиция пикселя и размеры изображения

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,24 @@
 <template>
-  <v-container class="fill-height">
     <v-col>
       <v-file-input accept="image/*" v-model="image" @change="uploadImage" label="Загрузить изображение"></v-file-input>
       <canvas @mousemove="getPixelData" id="myCanvas" style="border:1px solid #000000;"></canvas>
-      {{ colorData }}
+      <v-card class="d-none">
+      <v-card-item>
+        <v-card-title>{{ dimData }}</v-card-title>
+        <v-card-title>{{ posData }}</v-card-title>
+        <v-card-title>{{ colorData }}</v-card-title>
+      </v-card-item>
+    </v-card>
+      <footer>
+      <v-card>
+      <v-card-item>
+        <v-card-title>{{ dimData }}</v-card-title>
+        <v-card-title>{{ posData }}</v-card-title>
+        <v-card-title>{{ colorData }}</v-card-title>
+      </v-card-item>
+    </v-card>
+  </footer>
     </v-col>
-  </v-container>
 </template>
 
 <script>
@@ -14,6 +27,8 @@
     return {
       image: null,
       colorData: "",
+      posData: "",
+      dimData: "",
     }
   },
   methods: {
@@ -40,7 +55,9 @@
       var context = canvas.getContext("2d");
       var pos = this.getMousePos(canvas, evt);
       var pixelData = context.getImageData(pos.x, pos.y, 1, 1);
-      this.colorData = [pixelData.data[0], pixelData.data[1], pixelData.data[2]].toString();
+      this.colorData = "Цвет: " + [pixelData.data[0], pixelData.data[1], pixelData.data[2]].toString();
+      this.posData = "Позиция: " + [pos.x, pos.y].toString();
+      this.dimData = "Размер изображения: " + [canvas.width, canvas.height].toString();
     }
   }
 }
