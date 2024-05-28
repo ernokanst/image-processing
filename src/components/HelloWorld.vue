493926759bc8c91f73a482b1af3a32bfb08a2aeb
ernokanst <ernokanst@gmail.com>
2024-05-28 22:47:05 +0300

    Масштабирование изображения под canvas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,17 @@
 <template>
-    <v-col>
       <div id="upload">
+        <header>
+    <v-app-bar :elevation="6" color="primary">
+        <v-app-bar-title>
+          Обработка изображений
+        </v-app-bar-title>
+    </v-app-bar>
+  </header>
         <h1>Загрузите изображение</h1>
-        <v-file-input accept="image/*" v-model="image" @change="uploadImage" label="Загрузить изображение с компьютера"></v-file-input>
-        <v-text-field v-model="photoLink" @keydown.enter="getImage" label="Ссылка на изображение"></v-text-field>
+        <v-file-input accept="image/*" v-model="image" @change="imageFromFile" label="Загрузить изображение с компьютера"></v-file-input>
+        <v-text-field v-model="photoLink" @keydown.enter="imageFromWeb" label="Ссылка на изображение"></v-text-field>
       </div>
       <canvas @mousemove="getPixelData" id="myCanvas" style="border:1px solid #000000;"></canvas>
-      <v-card style="opacity: 0">
-      <v-card-item>
-        <v-card-title>{{ dimData }}</v-card-title>
-        <v-card-title>{{ posData }}</v-card-title>
-        <v-card-title>{{ colorData }}</v-card-title>
-      </v-card-item>
-      <v-card-actions>
-        <v-btn>Button</v-btn>
-      </v-card-actions>
-    </v-card>
     <v-dialog
       v-model="sizedialog"
       width="auto"
@@ -42,55 +38,72 @@
         </template>
       </v-card>
     </v-dialog>
-  </v-col>
       <footer>
-        <h2>{{ dimData }}</h2>
-        <h2>{{ posData }}</h2>
-        <h2>{{ colorData }}</h2>
+        <table><tr><h2>
+          <td id="dim">Размер изображения: </td>
+          <td>{{ posData }}</td>
+          <td>{{ colorData }}</td>
+        </h2></tr></table>
         <v-btn variant="text" @click="sizedialog = true">Изменить размер</v-btn>
   </footer>
 </template>
 
 <script>
+var img = new Image();
+var imgwidth = 0;
+var imgheight = 0;
   export default {
   data(){
     return {
       sizedialog: false,
       image: null,
-      colorData: "",
-      posData: "",
-      dimData: "",
+      colorData: "Цвет: ",
+      posData: "Позиция: ",
       photoLink: "",
     }
   },
   methods: {
-    getImage() { //Перед использованием включить https://cors-anywhere.herokuapp.com/
+    createImage() {
       var c = document.getElementById("myCanvas");
       var ctx = c.getContext("2d");
-      var img = new Image();
+      c.width = window.innerWidth;
+      c.height = window.innerHeight - $( "footer" ).height();
       img.onload = function(){
-        c.height = img.height;
-        c.width = img.width;
-        ctx.drawImage(img, 0, 0);
-        this.image = null;
+        if ((img.width <= (c.width - 100)) && (img.height <= (c.height - 100))) {
+          ctx.drawImage(img, c.width / 2 - img.width / 2, c.height / 2 - img.height / 2);
+          imgwidth = img.width;
+          imgheight = img.height;
+        } else {
+          var MAX_WIDTH = c.width - 100;
+          var MAX_HEIGHT = c.height - 100;
+          var width = img.width;
+          var height = img.height;
+          if (width > MAX_WIDTH) {
+            height = height * (MAX_WIDTH / width);
+            width = MAX_WIDTH;
+          }
+          if (height > MAX_HEIGHT) {
+            width = width * (MAX_HEIGHT / height);
+            height = MAX_HEIGHT;
+          }
+          imgwidth = width;
+          imgheight = height;
+          ctx.drawImage(img, c.width / 2 - width / 2, c.height / 2 - height / 2, width, height);
+        }
+        document.getElementById("dim").textContent = "Размер изображения: " + [img.width, img.height].toString();
       }
-      img.crossOrigin = "Anonymous";
-      img.src = "https://cors-anywhere.herokuapp.com/" + this.photoLink;
       document.getElementById("upload").style.display = 'none';
     },
-    uploadImage() {
-      var c = document.getElementById("myCanvas");
-      var ctx = c.getContext("2d");
-      var img = new Image();
-      img.onload = function(){
-        c.height = img.height;
-        c.width = img.width;
-        ctx.drawImage(img, 0, 0);
-      }
+    imageFromWeb() { //Перед использованием включить https://cors-anywhere.herokuapp.com/
+      this.createImage();
+      img.crossOrigin = "Anonymous";
+      img.src = "https://cors-anywhere.herokuapp.com/" + this.photoLink;
+    },
+    imageFromFile() {
+      this.createImage();
       var imgurl = URL.createObjectURL(this.image[0]);
       img.src = imgurl;
       this.photoLink = imgurl;
-      document.getElementById("upload").style.display = 'none';
     },
     getMousePos(canvas, evt) {
       var rect = canvas.getBoundingClientRect();
@@ -105,8 +118,11 @@
       var pos = this.getMousePos(canvas, evt);
       var pixelData = context.getImageData(pos.x, pos.y, 1, 1);
       this.colorData = "Цвет: " + [pixelData.data[0], pixelData.data[1], pixelData.data[2]].toString();
-      this.posData = "Позиция: " + [Math.floor(pos.x), Math.floor(pos.y)].toString();
-      this.dimData = "Размер изображения: " + [canvas.width, canvas.height].toString();
+      if ((pos.x >= canvas.width / 2 - imgwidth / 2) && (pos.x < canvas.width / 2 + imgwidth / 2) && (pos.y >= canvas.height / 2 - imgheight / 2) && (pos.y < canvas.height / 2 + imgheight / 2)) {
+        this.posData = "Позиция: " + [Math.floor(pos.x) - Math.floor(canvas.width / 2 - imgwidth / 2), Math.floor(pos.y) - Math.floor(canvas.height / 2 - imgheight / 2)].toString();
+      } else {
+        this.posData = "Позиция: ";
+      }
     }
   }
 }
