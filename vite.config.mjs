cd620c9def61d820fa6e3c6d54155ec03eca5730
ernokanst <ernokanst@gmail.com>
2024-09-16 12:21:37 +0300

    base для GH Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,6 +13,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/image-processing/",
   plugins: [
     VueRouter(),
     Layouts(),
