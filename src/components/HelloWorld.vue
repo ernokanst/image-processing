4505b17ad5bcf89b35ac0481424d03492920c758
ernokanst <ernokanst@gmail.com>
2024-09-16 11:25:11 +0300

    Лабораторная 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,11 +23,13 @@
       >
       <a>Оригинальный размер: {{ dimData }}</a>
       <a>Текущий масштаб: {{ scaleData }}%</a>
+      <v-select label="Единицы" v-model="sizeChoice" :items="sizeMeasures" @update:model-value="changeMeasure"></v-select>
       <a>Текущий размер:</a>
-      <v-text-field @mousemove="fixedRatioW" v-model="currentW" type="number" label="Ширина"></v-text-field>
-      <v-text-field @mousemove="fixedRatioH" v-model="currentH" type="number" label="Высота"></v-text-field>
+      <v-text-field @input="fixedRatioW" v-model="measureW" type="number" label="Ширина"></v-text-field>
+      <v-text-field @input="fixedRatioH" v-model="measureH" type="number" label="Высота"></v-text-field>
       <a>({{ newMP }} МПикс)</a>
       <v-checkbox label="Сохранить пропорции" v-model="preserveRatio"></v-checkbox>
+      <v-select label="Метод интерполяции" v-tooltip:top=interExplain v-model="interChoice" :items="interMethods" @update:model-value="changeInter"></v-select>
         <template v-slot:actions>
           <v-btn
             class="ms-auto"
@@ -51,6 +53,7 @@
         <v-row style="padding: 20px;">
         <v-slider v-model="scaleSlider" @update:model-value="changeImageSize" :max="300" :min="12" class="align-center" thumb-label></v-slider>
         <v-btn variant="text" @click="sizedialog = true">Изменить размер</v-btn>
+        <v-btn variant="text" @click="saveImage">Сохранить</v-btn>
       </v-row>
   </footer>
 </template>
@@ -73,16 +76,22 @@ var scale = 1;
       photoLink: "",
       scaleData: "",
       scaleSlider: "100",
-      currentW: "",
-      currentH: "",
       newMP: "",
+      measureW: "",
+      measureH: "",
+      sizeChoice: "Пиксели",
+      sizeMeasures: ["Пиксели", "Проценты"],
       preserveRatio: true,
+      interChoice: "Ближайшего соседа",
+      interMethods: ["Ближайшего соседа"],
+      interExplain: "Для каждого пикселя конечного изображения выбирается один пиксель исходного, наиболее близкий к его положению с учетом масштабирования.",
     }
   },
   methods: {
     createImage() {
       var c = document.getElementById("myCanvas");
       var ctx = c.getContext("2d");
+      c.style.imageRendering = "pixelated";
       c.width = window.innerWidth;
       c.height = window.innerHeight - $( "footer" ).height();
       img.onload = function(){
@@ -105,54 +114,98 @@ var scale = 1;
             width *= (MAX_HEIGHT / height);
             height = MAX_HEIGHT;
           }
-          imgwidth = width;
-          imgheight = height;
+          imgwidth = img.width;
+          imgheight = img.height;
           ctx.drawImage(img, c.width / 2 - width / 2, c.height / 2 - height / 2, width, height);
         }
         newimgwidth = imgwidth;
         newimgheight = imgheight;
         this.newMP = (imgwidth * imgheight / 1000000).toFixed(2);
-        document.getElementById("dim").textContent = "Размер изображения: " + [img.width, img.height].toString();
+        document.getElementById("dim").textContent = "Размер изображения: " + [Math.floor(img.width), Math.floor(img.height)].toString();
+        var sizes = [];
+        sizes[0] = ""
       }
-      document.getElementById("upload").style.display = 'none';
+      document.getElementById("upload").style.display = "none";
+    },
+    changeMeasure() {
+      if (this.sizeChoice == "Пиксели") {
+        this.measureW = Math.floor(imgwidth);
+        this.measureH = Math.floor(imgheight);
+      }
+      if (this.sizeChoice == "Проценты") {
+        this.measureW = (imgwidth / img.width * 100).toFixed(2);
+        this.measureH = (imgheight / img.height * 100).toFixed(2);
+      }
+    },
+    changeInter() {
+      if (this.sizeChoice == "Ближайшего соседа") {
+        this.interExplain = "Для каждого пикселя конечного изображения выбирается один пиксель исходного, наиболее близкий к его положению с учетом масштабирования.";
+      }
     },
     changeImageSize() {
       var c = document.getElementById("myCanvas");
       var ctx = c.getContext("2d");
+      c.style.imageRendering = "pixelated";
       ctx.clearRect(0, 0, c.width, c.height);
       scale = parseFloat(this.scaleSlider) / 100;
       imgwidth = newimgwidth * parseFloat(this.scaleSlider) / 100;
       imgheight = newimgheight * parseFloat(this.scaleSlider) / 100;
       this.newMP = (imgwidth * imgheight / 1000000).toFixed(2);
       ctx.drawImage(img, c.width / 2 - imgwidth / 2, c.height / 2 - imgheight / 2, imgwidth, imgheight);
-      document.getElementById("dim").textContent = "Размер изображения: " + [imgwidth.toFixed(2), imgheight.toFixed(2)].toString();
-      this.currentW = imgwidth.toFixed(2);
-      this.currentH = imgheight.toFixed(2);
+      document.getElementById("dim").textContent = "Размер изображения: " + [Math.floor(imgwidth.toFixed(2)), Math.floor(imgheight.toFixed(2))].toString();
+      this.changeMeasure();
     },
     updateSize() {
+      if (parseFloat(this.measureW) <= 0 || parseFloat(this.measureH) <= 0) {
+        alert("Размеры изображения не должны быть меньше или равны 0");
+        return;
+      }
       this.sizedialog = false;
       var c = document.getElementById("myCanvas");
       var ctx = c.getContext("2d");
+      c.style.imageRendering = "pixelated";
       ctx.clearRect(0, 0, c.width, c.height);
-      imgwidth = parseFloat(this.currentW);
-      imgheight = parseFloat(this.currentH);
+      if (this.sizeChoice == "Пиксели") {
+        imgwidth = parseFloat(this.measureW);
+        imgheight = parseFloat(this.measureH);
+      }
+      if (this.sizeChoice == "Проценты") {
+        imgwidth = img.width * parseFloat(this.measureW) / 100;
+        imgheight = img.height * parseFloat(this.measureH) / 100;
+      }
       scale = (imgwidth * imgheight) / (img.width * imgheight);
       newimgwidth = imgwidth / scale;
       newimgheight = imgheight / scale;
       ctx.drawImage(img, c.width / 2 - imgwidth / 2, c.height / 2 - imgheight / 2, imgwidth, imgheight);
-      document.getElementById("dim").textContent = "Размер изображения: " + [imgwidth.toFixed(2), imgheight.toFixed(2)].toString();
+      document.getElementById("dim").textContent = "Размер изображения: " + [Math.floor(imgwidth.toFixed(2)), Math.floor(imgheight.toFixed(2))].toString();
     },
     fixedRatioW() {
-      if (this.preserveRatio) {
-        this.currentH = String(imgheight * (parseFloat(this.currentW) / imgwidth));
+      if (this.sizeChoice == "Пиксели") {
+        if (this.preserveRatio) {
+          this.measureH = String(Math.floor(imgheight * (parseFloat(this.measureW) / imgwidth)));
+        }
+        this.newMP = (parseFloat(this.measureW) * parseFloat(this.measureH) / 1000000).toFixed(2);
       }
-      this.newMP = (parseFloat(this.currentW) * parseFloat(this.currentH) / 1000000).toFixed(2);
+      if (this.sizeChoice == "Проценты") {
+        if (this.preserveRatio) {
+          this.measureH = String(((imgheight / img.height).toFixed(2) * (parseFloat(this.measureW))).toFixed(2));
+        }
+        this.newMP = (parseFloat(imgwidth * (parseFloat(this.measureW) / 100)) * parseFloat(imgheight * (parseFloat(this.measureH) / 100)) / 1000000).toFixed(2);
+      }
     },
     fixedRatioH() {
-      if (this.preserveRatio) {
-        this.currentW = String(imgwidth * (parseFloat(this.currentH) / imgheight));
+      if (this.sizeChoice == "Пиксели") {
+        if (this.preserveRatio) {
+          this.measureW = String(Math.floor(imgwidth * (parseFloat(this.measureH) / imgheight)));
+        }
+        this.newMP = (parseFloat(this.measureW) * parseFloat(this.measureH) / 1000000).toFixed(2);
       }
-      this.newMP = (parseFloat(this.currentW) * parseFloat(this.currentH) / 1000000).toFixed(2);
+      if (this.sizeChoice == "Проценты") {
+        if (this.preserveRatio) {
+          this.measureW = String(((imgwidth / img.width).toFixed(2) * (parseFloat(this.measureH))).toFixed(2));
+        }
+        this.newMP = (parseFloat(imgwidth * (parseFloat(this.measureW) / 100)) * parseFloat(imgheight * (parseFloat(this.measureH) / 100)) / 1000000).toFixed(2);
+      }
     },
     imageFromWeb() { //Перед использованием включить https://cors-anywhere.herokuapp.com/
       this.createImage();
@@ -161,7 +214,7 @@ var scale = 1;
     },
     imageFromFile() {
       this.createImage();
-      var imgurl = URL.createObjectURL(this.image[0]);
+      var imgurl = URL.createObjectURL(this.image);
       img.src = imgurl;
       this.photoLink = imgurl;
     },
@@ -187,8 +240,19 @@ var scale = 1;
       this.scaleData = (scale * 100).toFixed(2);
       this.dimData = String(img.width) + ", " + String(img.height) + " (" + (img.width * img.height / 1000000).toFixed(2) + " МПикс)";
       this.newMP = (imgwidth * imgheight / 1000000).toFixed(2);
-      this.currentW = imgwidth.toFixed(2);
-      this.currentH = imgheight.toFixed(2);
+      this.changeMeasure();
+    },
+    saveImage() {
+      var c = document.createElement("canvas");
+      var ctx = c.getContext("2d");
+      c.style.imageRendering = "pixelated";
+      c.width = imgwidth
+      c.height = imgheight
+      ctx.drawImage(img, 0, 0, imgwidth, imgheight);
+      var link = document.createElement("a");
+      link.download = "image.png";
+      link.href = c.toDataURL()
+      link.click();
     }
   }
 }
